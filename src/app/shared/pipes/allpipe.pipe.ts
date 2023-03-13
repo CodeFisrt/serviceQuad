5b2d901f0953f150ba69627867c640b9a6f6cf39
akshata <akshatakale2468@gamil.com>
2023-03-13 16:23:23 +0530

    abc

--- src/app/shared/pipes/allpipe.pipe.ts
+++ src/app/shared/pipes/allpipe.pipe.ts
@@ -6,10 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class AllpipePipe implements PipeTransform {
 
   transform(value: unknown, ...args: unknown[]): unknown {
-    if (value == '' && value == null && value == undefined) {
-      return value = 'N/A';
-    } else {
+    if (value !== '' && value !== null && value !== undefined) {
       return value;
+    } else {
+      return value = 'NA';
     }
   };
 }
